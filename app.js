a72c507c4b2579e610f984db55924e1c35103d39
Your Name <agandhi29@myseneca.ca>
2022-09-22 21:24:24 -0400

    fixed the help message removed clone info

--- app.js
+++ app.js
@@ -3,28 +3,28 @@ import { program } from 'commander';
 import { ssg_ } from './index.js';
 
 program
-  .option('-v, --version', 'output the current version')
-  .option('-i, --input <item>', 'gets the input')
-  .option('-h, --help', 'help');
+	.option('-v, --version', 'output the current version')
+	.option('-i, --input <item>', 'gets the input')
+	.option('-h, --help', 'help');
 
 program.parse(process.argv);
 if (program.version) {
-  console.log('Name of the package: ' + ' ag-ssg');
-  console.log('Version details: 0.1.0');
+	console.log('Name of the package: ' + ' ag-ssg');
+	console.log('Version details: 0.1.0');
 }
 
 if (program.opts().help) {
-  console.log(
-    'To run the code in terminal \n' +
-      'Clone the repository into your local drive \n' +
-      'git clone  <repository url> <project_name> \n' +
-      'node app.js -i  <iteam>  select the file or directory\n' +
-      'node app.js -v --version to get the version details\n' +
-      'node app.js -h --help  to get the option \n' +
-      'Need to install all the dependencies such as npm  install , npm links , Build and run the project\n'
-  );
+	console.log(
+		'To run the code in terminal \n' +
+			'npm install \n' +
+			'npm link \n' +
+			'node app.js -i  <item>  select the file or directory\n' +
+			'node app.js -v --version to get the version details\n' +
+			'node app.js -h --help  to get the option \n' +
+			'Need to install all the dependencies such as npm  install , npm links , Build and run the project\n'
+	);
 }
 if (program.opts().input) {
-  console.log('input:' + program.opts().input);
-  ssg_(`${program.opts().input}`);
+	console.log('input:' + program.opts().input);
+	ssg_(`${program.opts().input}`);
 }
